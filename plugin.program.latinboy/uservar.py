7ee28250e6baa145734c270b91f9ace67ec7c074
latinboykodi <latinboykodi@gmail.com>
2024-12-17 13:52:10 -0400

    up

---
+++ plugin.program.latinboy/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://raw.githubusercontent.com/latinboykodi/latinboykodi.github.io/main/builds/builds.json'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://raw.githubusercontent.com/latinboykodi/latinboykodi.github.io/main/builds/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
